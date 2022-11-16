ce5544436c56d7d5e6265b4e982ed873102f500f
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-16 17:59:01 +0100

    adds array on html

--- js/main.js
+++ js/main.js
@@ -21,30 +21,58 @@ Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 
 */
 
+
+/*
+1 - stampare in html il contenuto dell"array 
+2 - aggiungere gli event listener sui pulsanti per andare avanti ed indietro
+3 - una variabile globale dove salvare l"indice dell"immagina attualmene attiva
+4 - al click sui pulsanti avanti e indietro, aggiorno l"indice e di conseguenza aggiorno l"html mostrando 
+l"immagine corrispondente.
+*/
+const sliderContainer = document.querySelector(".slider")
 const images = [
     {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+        image: "img/01.webp",
+        title: "Marvel\"s Spiderman Miles Morale",
+        text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
     },
     {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+        image: "img/02.webp",
+        title: "Ratchet & Clank: Rift Apart",
+        text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
     }, 
     {
-        image: 'img/03.webp',
-        title: 'Fortnite',
+        image: "img/03.webp",
+        title: "Fortnite",
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, 
     {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+        image: "img/04.webp",
+        title: "Stray",
+        text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
     }, 
     {
-        image: 'img/05.webp',
+        image: "img/05.webp",
         title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+        text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
     }
 ];
+
+const imgesSlider = images.map( function (imgEl) {
+    const sliderEl =`<div>
+                    ${imgEl.title}
+                    <img class="img-fluid" src="${imgEl.image}"/>
+                    ${imgEl.text}
+                    </div>`
+    sliderContainer.innerHTML += sliderEl;
+    return{
+       sliderEl,
+    };
+   
+
+    
+
+   
+}); 
+
+console.log(imgesSlider);
